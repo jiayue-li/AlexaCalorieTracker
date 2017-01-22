588054524df8277a58a67d42701590a938666b08
jiayue-li <jiayue-li>
2017-01-21 18:23:17 -0800

    Added Skeleton for Main File

---
+++ Main.js
@@ -0,0 +1,72 @@
+exports.handler = (event, context) =>
+{
+
+  try {
+    if (event.session.new) {
+      //New Session
+      console.log("NEW SESSION")
+    }
+
+    switch (event.request.type) {
+
+      case "LaunchRequest":
+        // > Launch Request
+        console.log("LAUNCH REQUEST")
+        context.succeed (
+          generateResponse(
+            buildSpeechletResponse("Welcome to Calorie Counter", true)
+          )
+        )
+        break;
+
+      case "IntentRequest":
+        // Intent Request
+        console.log('INTENT REQUEST')
+
+        switch(event.request.intent.name) {
+          case "CountCalories":
+            var calories = 300;
+            context.succeed(generateResponse(buildSpeechletResponse('You consumed ${calories}', true)))
+        }
+
+
+        break;
+
+      case "SessionEndRequest":
+        // Session Ended Request
+        console.log('SESSION ENDED REQUEST')
+        break;
+
+      default:
+        context.fail('INVALID REQUEST TYPE ${event.request.type}')
+
+      }
+  } catch(error) {context.fail('Exception: ${error}')}
+
+}
+
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+  return {
+    outputSpeech {
+      type: "PlainText",
+      text: outputText
+    },
+    shouldEndSession: shouldEndSession
+  }
+}
+
+generateResponse = (sessionAttributes, speechletResponse) => {
+  return {
+    version: "1.0",
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
+  }
+}
+
+    // > Launch Request
+    //
+
+
+
+
+}
