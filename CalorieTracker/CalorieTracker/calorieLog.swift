f0e30a11e8916dba87eeae77c23c37035cc329d4
jiayue-li <jiayue-li>
2017-01-22 10:24:07 -0800

    Added Log

--- CalorieTracker/CalorieTracker/calorieLog.swift
+++ CalorieTracker/CalorieTracker/calorieLog.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class caloriesLog: UIViewController{
+    @IBOutlet weak var numFoodsField: UITextField!
+    @IBOutlet weak var typeFoodsField: UITextField!
+
+    @IBOutlet weak var logField: UITextView!
+    
+    var logText = "Log:"
+    
+    @IBAction func addLog(_ sender: Any) {
+        var numFoods = numFoodsField.text
+        var typeFood = typeFoodsField.text
+        logText = logText + "\n" + numFoods! + " " + typeFood!
+        logField.text = logText
+
+    }
     
 }
